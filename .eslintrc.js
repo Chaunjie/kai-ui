b94dfdc246a33b108fbb32457728547bbd686713
徐程界 <xuchengjie@xuchengjiedeMacBook-Pro.local>
2018-08-01 09:46:31 +0800

    #0: add checkbox component

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'no-tabs': 'off',
+    'indent': [0]
   }
 }
